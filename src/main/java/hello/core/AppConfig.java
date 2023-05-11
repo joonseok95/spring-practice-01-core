18783631ee647fb550dda69a65137e3e2aa91133
jsp <esns79@outlook.com>
2023-05-11 14:03:48 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,11 +10,22 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
+    }
+
+
+
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static RateDiscountPolicy getDiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }

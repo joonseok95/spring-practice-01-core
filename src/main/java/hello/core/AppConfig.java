6613aebbae058ed422039ff1a105a31fdddf3414
jsp <esns79@outlook.com>
2023-05-11 14:00:38 +0900

    AppConfig 를 이용해서 DI

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,11 +10,21 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), DiscountPolicy());
+    }
+
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private static RateDiscountPolicy DiscountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
